3dce5f2c1375e31c66f1369a37e4cc8a90818568
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-07-20 11:39:57 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        hashmap = dict()
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                hashmap[nums[i]] += 1
+            else:
+                hashmap[nums[i]] = 1
+
+        j=0
+
+        if hashmap.get(0,0):
+            for _ in range(hashmap.get(0,0)):
+                nums[j] = 0
+                j+=1
+        if hashmap.get(1,0):
+            for _ in range(hashmap.get(1,0)):
+                nums[j] = 1
+                j+=1
+
+        if hashmap.get(2,0):
+            for _ in range(hashmap.get(2,0)):
+                nums[j] = 2
+                j+=1
